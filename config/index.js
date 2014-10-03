c39ee69e0048fb9ce85a37d5914ff7f291d57d12
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-10-03 16:10:08 +0800

    Config file

---
+++ config/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var fs = require('fs');
+
+var url = require('url');
+
+var nodeEnv = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3000;
+
+var mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/affiliate-parser-' + nodeEnv;
+
+var redisConfig;
+if (nodeEnv !== 'production') {
+  redisConfig = {
+    port: '6379',
+    host: 'localhost'
+  };
+} else {
+  var redisURL = url.parse(process.env.REDIS_URL);
+  redisConfig = {
+    port: redisURL.port,
+    host: redisURL.hostname,
+    auth: redisURL.auth.split(":")[1]
+  };
+}
+
+var creds;
+if (fs.existsSync('../keys.json')) {
+  creds = require('../keys.json');
+} else {
+  console.error('Impossible to retrieve file keys.json');
+  process.exit(1);
+}
+
+module.exports = {
+  env: nodeEnv,
+  port: port,
+
+  mongoUrl: mongoUrl,
+  redis: redisConfig,
+
+  creds: creds
+};
