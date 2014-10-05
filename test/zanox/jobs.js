197d507af16a32a1a97bbfd98be2ad20a7b07f0d
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-10-05 11:40:33 +0800

    Check if the next page is sent or not

--- test/zanox/jobs.js
+++ test/zanox/jobs.js
@@ -11,19 +11,37 @@ describe('[Zanox] Jobs', function () {
     done();
   });
 
-  it.only('should return the selected advertiser page', function(done) {
-    nock('https://marketplace.zanox.com')
-      .post('/zanox/affiliate/1156722/merchant-directory/xhr-directory/pageNumber/1/pageLength/100')
-      .replyWithFile(200, __dirname + '/files/listPage.html');
+  describe('Retrive advertisers page', function() {
+    it('should return the selected advertiser page', function(done) {
+      nock('https://marketplace.zanox.com')
+        .post('/zanox/affiliate/1156722/merchant-directory/xhr-directory/pageNumber/1/pageLength/100')
+        .replyWithFile(200, __dirname + '/files/listPage.html');
 
-    jobs.retriveAdvertisersPage(1, function(err, res) {
-      if (err) {
-        done(err);
-      }
+      jobs.retriveAdvertisersPage(1, function(err, res) {
+        if (err) {
+          done(err);
+        }
 
-      res.advertisers.should.have.a.lengthOf(2);
-      res.nextPage.should.have.be.eql(2);
-      done();
+        res.advertisers.should.have.a.lengthOf(2);
+        res.nextPage.should.have.be.eql(2);
+        done();
+      });
+    });
+
+    it('should not add nextPage property if last page', function(done) {
+      nock('https://marketplace.zanox.com')
+        .post('/zanox/affiliate/1156722/merchant-directory/xhr-directory/pageNumber/10/pageLength/100')
+        .replyWithFile(200, __dirname + '/files/lastListPage.html');
+
+      jobs.retriveAdvertisersPage(10, function(err, res) {
+        if (err) {
+          done(err);
+        }
+
+        res.advertisers.should.have.a.lengthOf(2);
+        res.should.not.have.property('nextPage');
+        done();
+      });
     });
   });
 });
