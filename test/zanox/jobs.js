010408b7aebac62fa027b9a51f1aba33b02bfaa3
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-10-07 13:22:33 +0800

    Dynamic parsing

--- test/zanox/jobs.js
+++ test/zanox/jobs.js
@@ -18,9 +18,6 @@ describe('[Zanox] Jobs', function () {
         .replyWithFile(200, __dirname + '/files/listPage.html');
 
       jobs.retriveAdvertisersPage(1, function(err, res) {
-        if (err) {
-          done(err);
-        }
 
         res.advertisers.should.have.a.lengthOf(2);
         res.nextPage.should.have.be.eql(2);
@@ -29,7 +26,7 @@ describe('[Zanox] Jobs', function () {
           return ad.should.have.properties('id', 'name');
         });
 
-        done();
+        done(err);
       });
     });
 
@@ -39,13 +36,11 @@ describe('[Zanox] Jobs', function () {
         .replyWithFile(200, __dirname + '/files/lastListPage.html');
 
       jobs.retriveAdvertisersPage(10, function(err, res) {
-        if (err) {
-          done(err);
-        }
 
         res.advertisers.should.have.a.lengthOf(2);
         res.should.not.have.property('nextPage');
-        done();
+
+        done(err);
       });
     });
   });
@@ -70,9 +65,6 @@ describe('[Zanox] Jobs', function () {
           id: 3124
         }
       }, function(err, res) {
-        if (err) {
-          return done(err);
-        }
 
         res.should.have.property('comissions');
         res.comissions.should.have.properties('defaultComissions', 'comissionsItems');
@@ -81,8 +73,37 @@ describe('[Zanox] Jobs', function () {
 
         res.should.have.property('details');
         res.details.should.have.properties('name', 'id', 'image', 'shortDescription', 'longDescription', 'contact', 'url', 'cookies');
-        done();
+        done(err);
       });
     });
+
+    it('should be able handle multi-level comissions', function(done) {
+      var offers = '<div id="merchantKeyFigures"><ul><li><strong>Lead</strong> EUR 0.50</li>';
+      offers += '<li><strong>Vente</strong> EUR 1.00 - EUR 19.00</li></ul></div>';
+
+      nock('https://marketplace.zanox.com')
+        .get('/zanox/affiliate/1156722/1193212/merchant-profile/3124')
+        .replyWithFile(200, __dirname + '/files/detailPage.html')
+        .get('/zanox/affiliate/1156722/1193212/merchant-profile/3124/commission-groups')
+        .reply(200, offers)
+        .get('/zanox/affiliate/1156722/1193212/merchant-profile/3124/xhr-commission-group-search/page/1')
+        .replyWithFile(200, __dirname + '/files/comissionsDetailVariable.html');
+
+      jobs.retrieveAdvertiserInfo({
+        advertiser: {
+          id: 3124
+        }
+      }, function(err, res) {
+
+        res.should.have.property('comissions');
+        res.comissions.comissionsItems.should.have.a.lengthOf(10);
+        res.comissions.comissionsItems.filter(function(item) {
+          return item['Lead (fix)'];
+        }).should.have.a.lengthOf(6);
+
+        done(err);
+      });
+    });
+
   });
 });
